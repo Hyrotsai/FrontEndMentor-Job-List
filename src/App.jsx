54b2c499a17cd46ba4801311b4f69900f53125e6
Luis Rodriguez <luis.rodriguez.simancas@gmail.com>
2024-01-14 17:31:16 -0500

    Fix SVG

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,18 @@ import JobList from './components/JobList';
 function App() {
   return (
     <section className='main__section'>
-      <div className='svg__background'>SVG AQUI</div>
+      <div className='svg__background'>
+        <img
+          src='../public/bg-header-desktop.svg'
+          alt='SVG Desktop'
+          className='svg__background__desktop'
+        />
+        <img
+          src='../public/bg-header-mobile.svg'
+          alt='SVG Desktop'
+          className='svg__background__mobile'
+        />
+      </div>
       <div className='content__section'>
         <JobList />
       </div>
